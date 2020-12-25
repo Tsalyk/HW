787f001354c14000aa27c98fa4ba430068d97120
Tsalyk <73395389+Tsalyk@users.noreply.github.com>
2020-12-25 09:56:15 +0200

    Update config.py

--- telegram_bot/config.py
+++ telegram_bot/config.py
@@ -4,12 +4,12 @@ TOKEN = '1470698716:AAHIFW3071MvHiyCvr9PVUjwvmnSQllqWL0'
 # text
 CONGRATULATIONS = ("Welcome, {0.first_name}!\U0000270B\nI "
                    "am a {1.first_name}, "
-                    "I will help you to create a "
-                    "password\U0001F60A")
+                   "I will help you to create a "
+                   "password\U0001F60A")
 INSTRUCTION = ("Here are available "
-                "commands\n\U00002B07\U00002B07\U00002B07\n\n"
-                "/gen_pass")
+               "commands\n\U00002B07\U00002B07\U00002B07\n\n"
+               "/gen_pass")
 PASSWORDS = ("Please, choose a type of your password:\n\n"
-            "1 - <b>easy</b> password\n"
-            "2 - <b>normal</b> password\n"
-            "3 - <b>strong</b> password")
+             "1 - <b>easy</b> password\n"
+             "2 - <b>normal</b> password\n"
+             "3 - <b>strong</b> password")
